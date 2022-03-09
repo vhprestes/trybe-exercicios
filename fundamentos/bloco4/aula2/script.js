b79e7a6a62d6e6535121a0e5affa0aabd93a6911
Victor Prestes Martins <vhprestes@gmail.com>
2022-03-08 22:08:45 -0300

    Exercicios do dia resolvidos

---
+++ fundamentos/bloco4/aula2/script.js
@@ -0,0 +1,107 @@
+//  Exercicio 1
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index++) {
+//     let element = numbers[index];
+//     console.log(element);
+// }
+
+
+// Exercicio 2
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let mark = 0
+// for (let index = 0; index < numbers.length; index++) {
+//         let element = numbers[index];
+//         mark = mark + numbers[index];
+//     console.log(mark);
+// }
+
+// Exercicio 3
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let mark = 0
+// for (let index = 0; index < numbers.length; index++) {
+//             mark = mark + numbers[index];
+//             if (numbers.length == index + 1) {
+//                  console.log( "A média é : ", mark / index);
+//             }
+   
+
+// }
+
+
+// Exercicio 4
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let mark = 0
+// for (let index = 0; index < numbers.length; index++) {
+//             mark = mark + numbers[index];
+//             if (numbers.length == index + 1) {
+//                  console.log( "A média é : ", mark / index);
+//                 if (mark / index > 20) {
+//                     console.log("A média é maior que 20");                    
+//                 }
+//                 }
+   
+
+// }
+
+// Exercicio 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maior = 0
+// maior = numbers[0];
+//     for (let index = 1; index < numbers.length; index++) {
+//         if (maior < numbers[index]) {
+//             maior = numbers[index];
+//                     }
+//     }            console.log(maior);
+
+// Exercicio 6
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let contImpar = 0
+
+// for (let index = 0; index < numbers.length; index++) {
+//     let element = numbers[index];
+//         if (numbers[index] % 2 == 1) {
+//             contImpar++
+            
+//         }
+//     }
+//     console.log(contImpar);
+
+
+// Exercicio 7
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let menor = 0
+// menor = numbers[0];
+//     for (let index = 1; index < numbers.length; index++) {
+//         if (menor > numbers[index]) {
+//             menor = numbers[index];
+//                     }
+//     }            console.log(menor);
+
+
+// Exercicio 8
+// let ronaldo = [];
+//     for (let i = 0; i < 26; i++)
+//      {      
+//                  ronaldo.push(i);        
+     
+//     }
+//     console.log(ronaldo)
+
+
+// Exercicio 9
+
+
+let ronaldo = [];
+    for (let i = 0; i < 26; i++)
+     {      
+                 ronaldo.push(i);        
+     console.log(ronaldo[i] / 2)
+    }
+    
