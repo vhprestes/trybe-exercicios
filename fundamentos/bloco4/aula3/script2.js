71cd208e64575f254da4e4e2d4c1f3b24e85e1b0
Victor Prestes Martins <vhprestes@gmail.com>
2022-03-09 21:18:58 -0300

    Exercicio 1 resolvido

---
+++ fundamentos/bloco4/aula3/script2.js
@@ -0,0 +1,12 @@
+// Exercicio 1
+// crie um algoritmo que retorne o fatorial de 10
+
+
+let fatorial = 10
+for (let index = fatorial - 1; index > 1; index--) {
+        fatorial = fatorial * index;
+        
+}
+console.log(fatorial);
+
+//
