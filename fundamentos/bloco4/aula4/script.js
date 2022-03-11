2508a2e201206e5de7c6b9f7ecbd2bd177c587c8
Victor Prestes Martins <vhprestes@gmail.com>
2022-03-11 02:48:38 -0300

    criacao dos arquivos da aula 4

---
+++ fundamentos/bloco4/aula4/script.js
@@ -0,0 +1,28 @@
+// Exercicio 1
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+//   console.log("Bem vinda, " + info.personagem);
+
+
+//   info.recorrente = 'sim';
+
+//   console.log(info);
+
+
+// for (const key in info) {
+//     console.log(key);
+        
+//     }
+
+
+
+for (const key in info) {
+    console.log(info[key]);
+        
+    }
