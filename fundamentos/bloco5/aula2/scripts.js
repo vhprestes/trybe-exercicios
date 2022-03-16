f07d58b626e6e1e4317f63f1d4c9c97c86bf0c47
Victor Prestes Martins <vhprestes@gmail.com>
2022-03-15 23:05:23 -0300

    termino primeira parte

--- fundamentos/bloco5/aula2/scripts.js
+++ fundamentos/bloco5/aula2/scripts.js
@@ -1,39 +1,73 @@
 // scripts
 
 //1
-let elemento = document.createElement('h1')
-elemento.innerText = 'Exercício 5.2 - JavaScript DOM ';
-document.body.appendChild(elemento)
-
+let elemento = document.createElement("h1");
+elemento.innerText = "Exercício 5.2 - JavaScript DOM ";
+document.body.appendChild(elemento);
 
 //2
-let main = document.createElement('main')
-main.classList = 'main-content'
-document.body.appendChild(main)
+let main1 = document.createElement("main");
+main1.classList = "main-content";
+document.body.appendChild(main1);
 
 //3
-let section = document.createElement('section')
-section.classList = 'center-content'
-main.appendChild(section)
+let section1 = document.createElement("section");
+section1.classList = "center-content";
+main1.appendChild(section1);
 
 //4
-let p1 = document.createElement('p')
-p1.innerText = 'Texto para o P aqui'
-section.appendChild(p1)
-
+let p1 = document.createElement("p");
+p1.innerText = "DEU CERTO PQ A LORENA ENSINOU";
+section1.appendChild(p1);
 
 //5
-let sectionleft = document.createElement('section')
-section.classList = 'left-content'
-main.appendChild(sectionleft)
+let sectionleft = document.createElement("section");
+sectionleft.classList = "left-content";
+main1.appendChild(sectionleft);
 
 //6
-let sectionright = document.createElement('section')
-section.classList = 'right-content'
-main.appendChild(sectionright)
+let sectionright = document.createElement("section");
+sectionright.classList = "right-content";
+main1.appendChild(sectionright);
 
 //7
-let image = document.createElement('img')
-section.classList = 'small-image'
-image.src = 'https://picsum.photos/200'
-sectionleft.appendChild(image)
+let image = document.createElement("img");
+image.classList = "small-image";
+image.src = "https://picsum.photos/200";
+sectionleft.appendChild(image);
+
+//8
+let lista = document.createElement("ul");
+let numeros = [
+  "um",
+  "dois",
+  "três",
+  "quatro",
+  "cinco",
+  "seis",
+  "sete",
+  "oito",
+  "nove",
+  "dez",
+];
+
+for (let index in numeros) {
+  let itensLista = document.createElement("li");
+  itensLista.innerText = numeros[index];
+  lista.appendChild(itensLista);
+}
+
+sectionright.appendChild(lista);
+
+//9
+function createElement(titulos) {
+  let tituloh3 = document.createElement("h3");
+  tituloh3.classList = "titulos";
+  return tituloh3;
+}
+  
+main1.appendChild(createElement("h31"));
+main1.appendChild(createElement("h32"));
+main1.appendChild(createElement("h33"));
+
+
